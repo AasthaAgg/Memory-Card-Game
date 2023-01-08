6403531aa5ee1156790eb8e8cc8fb5c84658f625
Aastha Aggarwal <aastha.7090@gmail.com>
2023-01-08 14:03:03 +0530

    card double click

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,9 @@ let firstCard, secondCard;
 function flipCard(){
     if(lockBoard) return;
 
+    // check for double click
+    if(this === firstCard) return;
+
     this.classList.add('flip');
 
     // first card
@@ -18,7 +21,6 @@ function flipCard(){
     }
 
     //second card
-    hasFlippedCard = false;
     secondCard = this;
 
     matchCards();
@@ -35,6 +37,8 @@ function matchCards(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unFlipCards(){
@@ -44,8 +48,13 @@ function unFlipCards(){
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockBoard = false;
+        resetBoard();
     }, 1000);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
